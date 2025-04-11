740b401796a53e784cc38cf3fe1194e63adf214f
laupontiroli <laurapm@al.insper.edu.br>
2025-04-10 21:12:39 -0300

    projeto

---
+++ Assets/Scripts_/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private  Rigidbody2D rb;
+    public float speed;   
+    AudioSource audio;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        audio = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.MovePosition(rb.position + movement.normalized * Time.fixedDeltaTime * speed);
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Coletavel")
+        {
+            audio.Play();
+            GameController.Collect();
+            Destroy(other.gameObject);
+        }
+    }
+}
